5b12e8066babc2d2cfeaa3a532cd783f24730811
EmmaWikstrom <emma.malmwikstrom@gmail.com>
2025-11-05 21:30:03 +0100

    Add overlay functionality to lightbox for improved user experience

--- src/lightbox.js
+++ src/lightbox.js
@@ -6,19 +6,20 @@ export const showLightbox = (imageUrl) => {
     lightbox.classList.remove('hidden');
     const lightboxImage = document.querySelector('.lightbox-image'); 
     lightboxImage.src = imageUrl;
+    const overlay = document.querySelector('.overlay');
+    overlay.classList.remove('hidden');
+
 };
 
 const closeLightbox = () => { 
     const lightbox = document.querySelector('.lightbox');
     lightbox.classList.add('hidden');
+    const overlay = document.querySelector('.overlay');
+    overlay.classList.add('hidden');
 };
 
 const lightboxCloseButton = document.querySelector('.lightbox-close-button'); 
 lightboxCloseButton.addEventListener('click', () => closeLightbox());
 
 
-// const images = Array.from(document.querySelectorAll('.image-container'));
-// images.forEach(image => {
-//     image.addEventListener('click', () => showLightbox())
-// });
 
