8a0ed254d96073544e3f1d437b9741f1c2ce148d
EmmaWikstrom <emma.malmwikstrom@gmail.com>
2025-11-05 16:10:34 +0100

    Add lightbox close button

--- src/lightbox.js
+++ src/lightbox.js
@@ -6,7 +6,13 @@ export const showLightbox = () => {
     lightbox.classList.remove('hidden');
 };
 
+export const closeLightbox = () => { 
+    const lightbox = document.querySelector('.lightbox');
+    lightbox.classList.add('hidden');
+};
 
+const lightboxCloseButton = document.querySelector('.lightbox-close-button'); 
+lightboxCloseButton.addEventListener('click', () => closeLightbox());
 // const images = Array.from(document.querySelectorAll('.image-container'));
 // images.forEach(image => {
 //     image.addEventListener('click', () => showLightbox())
