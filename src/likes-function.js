89fcf9dd0a91838e937a6f7360101872ffa624b7
Irem Stalbrand <iremstalbrand@gmail.com>
2025-11-10 16:23:13 +0100

    Refactor likeButtonOnClick to streamline logic and improve readability

--- src/likes-function.js
+++ src/likes-function.js
@@ -1,18 +1,21 @@
-import { findImageDataByID, state } from "./main.js";
+import { findImageDataByID } from "./main.js";
 
 export const likeButtonOnClick = (button) => {
-  button.classList.add("active");
-
   const likeNumber = button.children[1];
-  likeNumber.textContent = Number(likeNumber.textContent) + 1;
-
-  const imageContainer = button.parentElement.parentElement.parentElement;
+  const imageContainer = button.parentElement.parentElement.parentElement; //id is in the image container
   const ID = imageContainer.id;
-  console.log(ID);
+  const imageData = findImageDataByID(ID); //find object in state.imagesData with matching id
 
-  const imageData = findImageDataByID(ID);
-  imageData.likes_count++;
-
-  imageData.userLiked = true;
-  console.log(state.imagesData[0]);
+  if (!button.classList.contains("active")) {
+    //if button is not active - user has not clicked before
+    button.classList.add("active");
+    likeNumber.textContent = Number(likeNumber.textContent) + 1; //update like number in dom
+    imageData.likes_count++; //updates the data
+    imageData.userLiked = true; //stores in data that user liked the picture
+  } else {
+    button.classList.remove("active");
+    likeNumber.textContent = Number(likeNumber.textContent) - 1;
+    imageData.likes_count--;
+    imageData.userLiked = false; //unlike, removes the previous data
+  }
 };
