f7721fd605d3530705093019574cb10402835948
Irem Stalbrand <iremstalbrand@gmail.com>
2025-11-10 15:45:10 +0100

    Refactor likeButtonOnClick to improve readability and structure

--- src/likes-function.js
+++ src/likes-function.js
@@ -2,7 +2,17 @@ import { findImageDataByID, state } from "./main.js";
 
 export const likeButtonOnClick = (button) => {
   button.classList.add("active");
-  const imageContainer = button.parentElement.parentElement.parentElement;
 
-  console.log(imageContainer);
+  const likeNumber = button.children[1];
+  likeNumber.textContent = Number(likeNumber.textContent) + 1;
+
+  const imageContainer = button.parentElement.parentElement.parentElement;
+  const ID = imageContainer.id;
+  console.log(ID);
+
+  const imageData = findImageDataByID(ID);
+  imageData.likes_count++;
+
+  imageData.userLiked = true;
+  console.log(state.imagesData[0]);
 };
